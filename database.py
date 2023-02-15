1e21e26304690cab44e70447562ff71975d59132
Yoriel Silva <yoriel.silva.rodrigues@gmail.com>
2023-02-15 13:37:41 -0300

    finalição da integração basica

--- database.py
+++ database.py
@@ -1,12 +1,24 @@
 import pyodbc
 
 dados = (
-    "Driver={****};"
-    "User=****;"
-    "Password=****;"
-    "Server=****;"
-    "DataBase=****;"
+    "Driver={driver};"
+    "User=your_user;"
+    "Password=your_pass;"
+    "Server=your_server;"
+    "DataBase=your_database;"
 )
+
+nome = str(input('Qual o seu nome? '))
+usuario = str(input('Qual será seu nome de usuario? '))
+senha = str(input('Qual será sua senha? '))
 
 conexao = pyodbc.connect(dados)
 print("yes")
+
+cursor = conexao.cursor()
+
+comando = f"""insert into dados values(
+    default, '{nome}', '{usuario}', '{senha}');"""
+
+cursor.execute(comando)
+cursor.commit()
